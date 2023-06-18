8b7c62a9f7e725c91cfd396370c514cbda3f35c8
Mallikarjuna <mallikarjuna.c@aylanetworks.com>
2023-06-18 18:48:10 +0530

    Sample clock app in swiftUI

---
+++ SampleClockApp/SampleClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleClockAppApp.swift
+//  SampleClockApp
+//
+//  Created by Malli on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
